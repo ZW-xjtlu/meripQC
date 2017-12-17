28c59ee704b40384b221da9a90322eb4b8626bc4
ZhenWei <zhenwei@ZhenWeideMacBook-Pro.local>
2017-12-17 12:07:26 +0800

    minor correction
    quik

--- R/Plot_ex_lengths.R
+++ R/Plot_ex_lengths.R
@@ -14,7 +14,7 @@ Plot_ex_lengths <- function(LIST_GR,TXDB,HDER){
 ex_txdb <- exons(TXDB)
 LST_exlengths <- lapply(LIST_GR, function(x) width( subsetByOverlaps( ex_txdb, x) ) )
 plot_df <- data.frame(log2_exon_lengths = log2( unlist(LST_exlengths,use.names = FALSE) ),
-           Group = rep(names(LST_exlengths),elementNROWS(LST_exlengths)))
+           Group = rep(names(LST_exlengths),S4Vectors::elementNROWS(LST_exlengths)))
 p1 <- ggplot(plot_df,aes(x = log2_exon_lengths, fill = Group)) + geom_density(alpha = .5,linetype = 0) + theme_classic() + labs(x = "log2 exon lengths", title = "exon length distribution", subtitle = HDER)
 ggsave(paste0(HDER,"_exl_dist.pdf"),p1,width = 5,height = 2.6)
 }
