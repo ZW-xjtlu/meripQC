30a146ea9f7b9f8df6d026cc5331f5ff6ed20aa5
ZhenWei10 <zhen.wei10@icloud.com>
2018-03-23 14:42:19 +0800

    Add another function plot_column_joint
    aaa

--- R/Plot_column_joint.R
+++ R/Plot_column_joint.R
@@ -2,8 +2,8 @@
 #'
 #' @description \code{Plot_column_joint} is a function used to non-supervised learn / cluster the relationship between columns. (Insight into the joint distribution between samples)
 #' @param M A \code{matrix}.
-#' @param METRIC Can be either "euclidean" or "pearson". The later is simply the euclidean distance after rescale the columns.
-#' @param VISUAL Can be either "dendrogram" or "MDS".
+#' @param METRIC The metric used in clustering, can be one in "euclidean", "pearson", and "binary". The pearson metric is calculated as the euclidean metric after rescaling the columns.
+#' @param CLUSTER The clustering method, can be one in "MDS" and  "dendrogram".
 #' @param HDER The subtitle and the file name of the plot.
 #' @param GROUP_LABEL Optional, a vector used for colour labeling or faceting the dendrogram or MDS plot.
 #'
@@ -14,8 +14,8 @@
 #' @examples
 #' Matrix_ex <- matrix(rnorm(9000),300,30)
 #' Group_lab <- paste0( "V_",rep(1:10,each = 3) )
-#' Plot_column_joint( Matrix_ex, "density", "Test1", Group_lab )
-#' Plot_column_joint( Matrix_ex, "box", "Test2", Group_lab )
+#' Plot_column_joint( Matrix_ex, "euclidean", "dendrogram", "Test1", Group_lab )
+#' Plot_column_joint( Matrix_ex, "pearson", "MDS", "Test2", Group_lab )
 #'
 #' @seealso \code{\link{Plot_column_marginal}}
 #'
@@ -25,30 +25,82 @@
 #' @importFrom reshape2 melt
 #' @export
 
-Plot_column_joint <- function(M, METRIC = "euclidean",VISUAL = "dendrogram", HDER = "", GROUP_LABEL = NULL){
-  stopifnot(METRIC %in% c("euclidean","pearson"))
+Plot_column_joint <- function(M, METRIC = "euclidean",VISUAL = "MDS", HDER = "", GROUP_LABEL = NULL){
+  stopifnot(METRIC %in% c("euclidean","pearson","binary"))
   stopifnot(VISUAL %in% c("dendrogram","MDS"))
+  stopifnot(length(HDER) == 1)
+
   if(is.null(colnames(M))) {colnames(M) = paste0("V_",seq_len(ncol(M)))}
 
-  require("ggdendro")
-  rownames(dist_matrix) = Clean_head(rownames(dist_matrix))
-  rownames(dist_matrix) = Clean_head(rownames(dist_matrix))
+  if(!is.null(GROUP_LABEL)) {stopifnot( is.vector( GROUP_LABEL ) & (length(GROUP_LABEL) ==  ncol(M)) )} else{
+    GROUP_LABEL = colnames(M)
+  }
 
-  dend <- as.dendrogram(hclust(as.dist(dist_matrix)))
+  if(METRIC == "pearson"){
+  dist_matrix <- dist( t(scale(M)), method = "euclidean")
+  } else {
+  dist_matrix <- dist( t(M), method = METRIC )
+  }
+
+  if (VISUAL == "dendrogram") {
+
+  dend <- hclust(dist_matrix)
 
   dend_data <- dendro_data(dend, type = "rectangle")
 
-  dend_data$labels[["Writer"]] = Extract_Role(dend_data$labels[["label"]])
+  yrange <- range(dend_data$segments$y)
+
+  ywidth <- (yrange[2]-yrange[1])
 
   p <- ggplot(dend_data$segments) +
-    geom_segment(aes(x = x, y = y, xend = xend, yend = yend))+
-    geom_text(data = dend_data$labels, aes(x, y - .02, label = label, colour = Writer),
-              hjust = 1, angle = 90, size = 2.3)+
-    ylim(-1, 1.2) + theme_classic() + labs(x ="", y = "height", title = "Dendrogram of differential methylations", subtitle = Name) + scale_color_brewer(palette = "Dark2")
-  ggsave(paste0(Name,"_dg.pdf"),p,height = 3.2,width = 4.6)
-  Dendrogram_Plot(sqrt_dist_matrix,Name = "sqrt_asym_dist")
+    geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) +
+    geom_text(data = dend_data$labels,
+              aes(x, y - .02, label = label, colour = GROUP_LABEL),
+              hjust = 1.05, angle = 90, size = 2.3) +
+    ylim(-3 -.5 * max(nchar(as.character(colnames(M)))),
+         yrange[2]+.25*ywidth) + theme_classic() +
+    labs(x ="", y = "height",
+         title = paste0("Collumn dendrogram with metric: ", METRIC),
+         subtitle = HDER)
 
+  p <- p + theme(plot.margin = margin(1,5,1,5,"cm"),
+                   legend.position = "bottom",
+                   legend.direction = "horizontal",
+                   legend.box = "vertical",
+                   legend.justification = "center",
+                   axis.text.x = element_text(angle = 310, vjust =.9, hjust = .1))
 
+  figwidth = 5 + 0.08 * max(5,max(nchar(as.character( colnames(M) )))) + .25* length( unique(colnames(M)) )
+
+  figheight = 2.65 + 0.1* max(5,max(nchar(as.character( colnames(M))))) + .1 * length( unique(colnames(M)) )
+
+  ggsave(paste0(HDER,"_dg.pdf"),p, height = figheight,width = figwidth)
+
+  }
+
+  if (VISUAL == "MDS"){
+
+    cmd_scale <- cmdscale(dist_matrix)
+
+    plot_df = data.frame(PC_1 = cmd_scale[,1],
+                         PC_2 = cmd_scale[,2],
+                         Group = GROUP_LABEL)
+
+    p1 <- ggplot(plot_df,aes(x = PC_1,y=PC_2,colour = Group)) +
+      geom_point(size = 2, alpha = .8) +
+      theme_classic() +
+      theme(plot.margin = margin(t = 2, r = 3, b = 2, l = 3.5, unit = "cm"),
+            legend.position = "bottom",legend.box = "vertical") +
+      labs(x = "First dimmension",
+           y="Second dimmension",
+           title = paste0("Collumn 2D plot with metric: ", METRIC),
+           subtitle = HDER) +
+      ylim(max(plot_df$PC_2) +1 ,min(plot_df$PC_2) - 1)
+
+    figheight =  4.5 + .12* ceiling(length( unique(GROUP_LABEL) )/5)
+    ggsave(paste0(HDER,"_mds.pdf"),p1,width = 6.35,height = figheight)
+
+  }
 
 }
 
